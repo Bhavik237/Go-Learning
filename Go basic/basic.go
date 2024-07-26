690ab7c27cd7eff0d62ba58e6b7432152c99d443
Bhavik Patel <bhavik.patel@tekditechnologies.com>
2024-07-27 00:21:15 +0530

    loop for intialize

--- Go basic/basic.go
+++ Go basic/basic.go
@@ -12,4 +12,20 @@ func main() {
 	//use of import statement
 	fmt.Println(quote.Go())
 
+	//declare in go
+	a := 2
+	fmt.Println(a)
+
+	count := 10
+
+	//for loop in Go 
+	for i := 0; i < count; i++ {
+		
+		// If statement in Go
+		if i == 5 {
+			fmt.Println("Goooooo")
+			continue
+		}
+		fmt.Println(i)
+	}
 }
